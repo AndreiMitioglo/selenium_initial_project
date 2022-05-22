116c1130ed58ff3e7d9a29b7633bf469b930d6a6
AndreiMitioglo <mitioandrew2@gmail.com>
2022-05-22 09:43:55 -0500

    add

--- src/TestCase4.java
+++ src/TestCase4.java
@@ -1,9 +1,8 @@
-package utilities;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import utilities.Driver;
 
 import java.util.List;
 
