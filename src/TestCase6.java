572de265257e02a8eef3bda59ecb3a295b4e3a2f
AndreiMitioglo <mitioandrew2@gmail.com>
2022-05-22 19:23:27 -0500

    updated

--- src/TestCase6.java
+++ src/TestCase6.java
@@ -1,8 +1,7 @@
-package utilities;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import utilities.Driver;
 
 public class TestCase6 {
     public static void main(String[] args) throws InterruptedException {
